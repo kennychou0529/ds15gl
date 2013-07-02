c82055bcb41e3a88615df81e2db0ea8493ae3a02
Johnson Tan <phisiart@gmail.com>
2013-07-02 08:59:15 +0800

    light and picture

--- ds15gl/dslight.cpp
+++ ds15gl/dslight.cpp
@@ -5,9 +5,8 @@ void dsSetLight() {
 
 	glEnable(GL_LIGHT0);
 
-	// 环境光
-	// GLfloat ambient[4]  = {0.5f, 250.0f / 255 / 2, 200.0f / 255 / 2, 0.5f};
-	GLfloat ambient[4]  = {1.0f, 1.0f, 1.0f, 0.5f};
+	// 环境光，这个得设置得暗一些，我们设成暗淡的白色
+	GLfloat ambient[4]  = {0.1f, 0.1f, 0.1f, 1.0f};
 
 	// 漫射光
 	GLfloat diffuse[4]  = {0.5f, 250.0f / 255 / 2, 200.0f / 255 / 2, 0.5f};
@@ -18,7 +17,7 @@ void dsSetLight() {
 	// GLfloat specular[4] = {0.0f, 0.0f, 0.0f, 1.0f};
 
 	// 光源位置
-	GLfloat position[4] = {1.0f, 0.0f, 1.0f, 0.5f};
+	GLfloat position[4] = {5.0f, 0.0f, 5.0f, 1.0f};
 	
 	// 将数据载入光源 0
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
@@ -26,19 +25,20 @@ void dsSetLight() {
 	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
 
-	// 聚光灯
+	/* 聚光灯
 	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 60.0f);
 	GLfloat spot_direction[] = {- position[0], - position[1], - position[2]};
-	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+	 GLfloat spot_direction[] = {0.0, 0.0, -1};
+	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);*/
 
 }
 
 void dsSetMaterial() {
 	// 环境光
-	GLfloat ambient[4]  = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat ambient[4]  = {1.0f, 1.0f, 1.0f, 10.0f};
 
 	// 漫射光
-	GLfloat diffuse[4]  = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat diffuse[4]  = {1.0f, 1.0f, 1.0f, 10.0f};
 
 	// 镜面光，产生亮斑
 	GLfloat specular[4] = {1.0f, 1.0f, 1.0f, 1.0f};
@@ -50,5 +50,5 @@ void dsSetMaterial() {
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
 	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shinness);
-	//glMaterialfv(GL_FRONT, GL_POSITION, position);
+
 }
