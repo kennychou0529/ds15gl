0827e06bce483248a59172a5b556a459ac6297b1
Johnson Tan <phisiart@gmail.com>
2013-09-05 20:05:56 +0800

    尝试 AStyle 自动风格化

--- ds15gl/dsActorManager.cpp
+++ ds15gl/dsActorManager.cpp
@@ -1,71 +1,71 @@
 #include "dsActorManager.h"
 
 
-DSActorManager::DSActorManager(void)
-{
-	round = 0;
+DSActorManager::DSActorManager(void) {
+    round = 0;
 }
 
 
-DSActorManager::~DSActorManager(void)
-{
+DSActorManager::~DSActorManager(void) {
 }
 
 
 //¼ÓÔØÈËÎï
-void DSActorManager::initialize(){
+void DSActorManager::initialize() {
 
-	list.insert(std::make_pair("sword_man", dsSoldier()));
-	list["sword_man"].load("data/sword_man/tris.md2",
-		"data/sword_man/tris.bmp",
-		"data/sword_man/weapon.md2",
-		"data/sword_man/weapon.bmp");
+    list.insert(std::make_pair("sword_man", dsSoldier()));
+    list["sword_man"].load("data/sword_man/tris.md2",
+                           "data/sword_man/tris.bmp",
+                           "data/sword_man/weapon.md2",
+                           "data/sword_man/weapon.bmp");
 
-	list["sword_man"].setPosition(6, 6);
-	list["sword_man"].setTarget(8, 9);
-	list["sword_man"].enterStatus(dsSoldier::Status::running);
+    list["sword_man"].setPosition(6, 6);
+    list["sword_man"].setTarget(8, 9);
+    list["sword_man"].enterStatus(dsSoldier::Status::running);
 
-	list.insert(std::make_pair("mage", dsSoldier()));
-	list["mage"].load("data/mage/tris.md2",
-		"data/mage/tris.bmp",
-		"data/mage/weapon.md2",
-		"data/mage/weapon.bmp");
+    list.insert(std::make_pair("mage", dsSoldier()));
+    list["mage"].load("data/mage/tris.md2",
+                      "data/mage/tris.bmp",
+                      "data/mage/weapon.md2",
+                      "data/mage/weapon.bmp");
 
-	list["mage"].setPosition(1, 1);
-	list["mage"].enterStatus(dsSoldier::Status::idle);
+    list["mage"].setPosition(1, 1);
+    list["mage"].enterStatus(dsSoldier::Status::idle);
 }
 //äÖÈ¾
-void DSActorManager::render(){
-	update();
-	if (list.empty()) return;
-	
-	glPushMatrix();
-	{
+void DSActorManager::render() {
+    update();
+    if (list.empty()) {
+        return;
+    }
+
+    glPushMatrix();
+    {
         for (auto& soldier : list) {
             soldier.second.animate();
         }
-	}
-	glPopMatrix();
+    }
+    glPopMatrix();
 }
 
-void DSActorManager::update(){
-	if(timer.getDurationMiliseci()>ROUNDTIME){
-		round++;
-		
-		while (script.notEmpty()&&script.nextRound() == round)
-		{
-			Record& record = script.getNextRecord();
-			SOLDIERS::iterator it=list.find(record.id);
-			if(it==list.end()) continue;
-			switch (record.type)
-			{
-			case soldier_move:
-				list[record.id].setTarget(record.x,record.y);
-				list[record.id].enterStatus(dsSoldier::Status::running);
-			default:
-				break;
-			}
-		}
-		timer.recordTime();
-	}
+void DSActorManager::update() {
+    if (timer.getDurationMiliseci() > ROUNDTIME) {
+        round++;
+
+        while (script.notEmpty() && script.nextRound() == round) {
+            Record& record = script.getNextRecord();
+            SOLDIERS::iterator it = list.find(record.id);
+            if (it == list.end()) {
+                continue;
+            }
+            switch (record.type) {
+            case soldier_move:
+                list[record.id].setTarget(record.x, record.y);
+                list[record.id].enterStatus(dsSoldier::Status::running);
+            default:
+                break;
+            }
+        }
+        timer.recordTime();
+    }
 }
