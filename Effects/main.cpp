672e8e686788a1db815806969754a26a9f0601f3
Sengent <wangkang-ziz@163.com>
2013-10-16 20:19:27 +0800

    粒子特效2

--- Effects/main.cpp
+++ Effects/main.cpp
@@ -1,58 +1,116 @@
 #include <GL/glut.h>
 #include <iostream>
-#include "particle.h"
+#include "tparticle.h"
 
 using namespace std;
-Emitter em;
+tEmitter* em;
+//tParticle* m_ParticlePool;
 
-void dsDisplay() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	gluLookAt(0,0,50,0,0,0,0,1,0);
-
-	em.draw();
-
-	glutSwapBuffers();
-
-	// 打印 GL 错误
-	GLenum errCode;
-	const GLubyte* errString;
-
-	while ((errCode = glGetError()) != GL_NO_ERROR) {
-		errString = gluErrorString(errCode);
-		std::cerr << errString << std::endl;
-	}
+void renderParticles(tEmitter* emitter) {
+    if (emitter == NULL) {
+        return;
+    }
+    addParticle(emitter);
+    tParticle* p = emitter->particle->next;
+	glPointSize(2.0f);
+    glBegin(GL_POINTS);
+    while (p != NULL) {
+        updateParticle(p, emitter);
+        glColor3f(p->color.r, p->color.g, p->color.b);
+        glVertex3f(p->pos.x, p->pos.y, p->pos.z);
+        p = p->next;
+    }
+    glEnd();
 }
 
 
+void dsDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0, 0, 50, 0, 0, 0, 0, 1, 0);
+
+    //extern void renderParticles(tEmitter* emitter);
+    //updateParticle()
+    renderParticles(em);
+
+    glutSwapBuffers();
+
+    // 打印 GL 错误
+    GLenum errCode;
+    const GLubyte* errString;
+
+    while ((errCode = glGetError()) != GL_NO_ERROR) {
+        errString = gluErrorString(errCode);
+        std::cerr << errString << std::endl;
+    }
+}
+
+void init() {
+    em = new tEmitter;
+    em->emitsPerFrame = 1000;
+    em->emitVar = 50;
+    em->endColor.r = 0;
+    em->endColor.g = 0;
+    em->endColor.b = 1;
+    em->flags  = 11;
+    em->force.x = 0;
+    em->force.y = 0;
+    em->force.z = 0;
+    em->id  = 11;
+    em->life = 100;
+    em->lifeVar = 50;
+    //em->name="test";
+    em->particleCount = 0;
+    em->pitch = 1;
+    em->pitchVar = 0.5f;
+    em->yaw = 1;
+    em->yaw = 0.5f;
+    em->pos.x = 0;
+    em->pos.y = 0;
+    em->pos.z = 0;
+    em->speed = 0.1;
+    em->speedVar  = 0.01;
+    em->totalParticles = 100000;
+    em->startColor.r = 1;
+    em->startColor.g = 1;
+    em->startColor.b = 0;
+    em->startColorVar.r = 0;
+    em->startColorVar.g = 0;
+    em->startColorVar.b = 0;
+	em->endColorVar.r = 0;
+	em->endColorVar.g = 0;
+	em->endColorVar.b = 0;
+    em->particle = NULL;
+    initParticlePool();
+}
 
 // 当窗口大小被修改时自动调用此函数
 void dsReshape(int w, int h) {
-	
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60, 4/3.0f, 2, 200);
-	glMatrixMode(GL_MODELVIEW);
-	glutPostRedisplay();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, 4 / 3.0f, 2, 200);
+    glMatrixMode(GL_MODELVIEW);
+    glutPostRedisplay();
 }
 
 void dsIdle() {
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 
-int main(int argc,char** argv){
-	glutInit(&argc,argv);
-	
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("DS 15th Effect Test");
-	glutDisplayFunc(dsDisplay);
-	
-	glutReshapeFunc(dsReshape);
-	glutIdleFunc(dsIdle);
-	
-	glutMainLoop();
-	
-	return 0;
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("DS 15th Effect Test");
+    glutDisplayFunc(dsDisplay);
+
+    glutReshapeFunc(dsReshape);
+    glutIdleFunc(dsIdle);
+    init();
+    glutMainLoop();
+
+    return 0;
 }
---
+++ Effects/tparticle.h
@@ -0,0 +1,83 @@
+﻿#ifndef _T_PARTIVCLE_H
+#define _T_PARTIVCLE_H
+
+
+#include <cmath>
+#include <iostream>
+#include <ctime>
+
+struct tVector {
+    float x, y, z;
+};
+struct tColor {
+    float r, g, b;
+};
+
+struct tParticle{
+
+    tParticle* prev, *next; // 双向链表， 指向前前驱结点和后继结点
+
+    tVector pos; // 当前位置
+
+    tVector prevPos;//之前位置
+
+    tVector dir; // 当前方向和速度
+
+    int life; // 还会存在的时间
+
+    tColor color; //粒子当前颜色
+
+    tColor prevColor; //粒子之前颜色
+
+    tColor deltaColor; //颜色改变量
+
+};
+
+struct tEmitter{
+
+    long id; //EMITTER ID
+
+    char name[80];// EMITTER NAME
+
+    long flags; //EMITTER FLAGS
+
+    //TRANSFORMATION INFO
+
+    tVector pos; //XYZ POSITION
+
+    float yaw, yawVar;// YAW AND VARIATION
+
+    float pitch, pitchVar; // PITCH AND VARIATION
+
+    float speed, speedVar;
+
+    // Particle
+
+    tParticle* particle; // NULL TERMINATED LINKED LIST
+
+    int totalParticles; // TOTAL EMITTED AT ANY TIME
+
+    int particleCount; // TOTAL EMITTED RIGHT NOW
+
+    int emitsPerFrame, emitVar; // EMITS PER FRAME AND VARIATION
+
+    int life, lifeVar; // LIFE COUNT AND VARIATION
+
+    tColor startColor, startColorVar; // CURRENT COLOR OF PARTICLE
+
+    tColor endColor, endColorVar; // CURRENT COLOR OF PARTICLE
+
+    // Physics
+
+    tVector force; //GLOBAL GRAVITY, WIND, ETC.
+
+};
+
+
+extern void RotationToDirection(float pitch, float yaw, tVector* direction) ;
+extern bool addParticle(tEmitter* emitter);
+extern bool updateParticle(tParticle* particle, tEmitter* emitter);
+//extern void renderParticles(tEmitter* emitter);
+extern void initParticlePool();
+
+#endif
